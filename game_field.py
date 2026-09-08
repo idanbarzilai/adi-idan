54d03de2bbaaff9a0bf8617525dbb170b0ff8155
idanbarzilai <idanbarzilai151@gmail.com>
2026-09-08 16:20:51 +0300

    game_field_flag_edit_and_get_w_h_edit

--- game_field.py
+++ game_field.py
@@ -19,21 +19,20 @@ def create_game_field():
     for row in range(consts.BOARD_ROWS): #אורך המטריצה
         game_field_matrix.append(create_row_game_field()) #מוסיף שורה ריקה לרשימה
 
-
+#יצירת דגל
 def create_flag ():
 
-    top_row = consts.BOARD_ROWS - consts.FLAG_ROWS #משתנה ששומר את הגובה של הדגל במטריצה
-    top_col = consts.BOARD_COLS - consts.FLAG_COLS #משתנה ששומר את רוכב הדגל במטריצה
 
     for row in range(len(game_field_matrix) - 1 , -1 , -1 ):
 
-        if top_row <= (row + 1): #בודק אם הגעתי עברתי את המיקום האחרון
+        if consts.FLAG_ROW <= (row + 1): #בודק אם הגעתי עברתי את המיקום האחרון
             break
 
         for col in range(len(game_field_matrix[row]) - 1 , -1 , -1 ):
-            if top_col <= (col + 1): #בודק אם הגעתי ועברתי את המיקום האחרון בשורה
+            if consts.FLAG_COL <= (col + 1): #בודק אם הגעתי ועברתי את המיקום האחרון בשורה
                 break
             game_field_matrix[row][col] = consts.FLAG_SQUARE #משתנה של דגל
+
 
 #יוצר מוקש במיקום רנדומאלי מציב אותו ברשימה ומחזיר מילון
 def create_mine():
@@ -89,10 +88,10 @@ def get_y(row):#מקבל באיזה שורה זה נמצא ומחזיר את מ�
     return (row * consts.CELL_SIZE)
 
 
-def get_w(x , num_col):#מקבל את מיקום הx של האובייקט ומחזיר את הרוחב שלו (ערך הx בפינה הימנית)
-    return (x * num_col)
+def get_w(x , num_col):#מקבל את מיקום הx של האובייקט ומחזיר את הרוחב שלו
+    return ((x * num_col) - get_x(num_col))
 
 
-def get_h(y, num_row):# מקבל את המיקום y  של האובייקט ומחזיר את הגובה שלו, בערך של y בקצה התחתון של האובייקט
-    return (y * num_row)
+def get_h(y, num_row):# מקבל את המיקום y  של האובייקט ומחזיר את הגובה שלו
+    return ((y * num_row) - get_y(num_row) )
 
