b7ed7e5d4f7406d07b82045e8d8fb89d13691a5c
jbt <jbt@114-04>
2026-09-08 12:28:01 +0300

    current consts

---
+++ soldier.py
@@ -0,0 +1,27 @@
+import consts
+
+#changing img to the right size
+def body_initial_place():
+    consts.BODY_PLACE[0] = [0,0]
+    consts.BODY_PLACE[1] = [0,1]
+    consts.BODY_PLACE[2] = [1,0]
+    consts.BODY_PLACE[3] = [1,1]
+    consts.BODY_PLACE[4] = [2, 0]
+    consts.BODY_PLACE[5] = [2, 1]
+
+def legs_initial_place():
+    consts.LEGS_PLACE[0] = [3,0]
+    consts.LEGS_PLACE[1] = [3,1]
+
+#creating soldier
+def create_soldier(center_x, center_y, color):
+    body_initial_place()
+
+    return {"place": consts.SOLDIER_START_PLACE,
+            "center_x": center_x,
+            "center_y": center_y,
+            "radius": consts.BUBBLE_RADIUS}
+
+
+
+
